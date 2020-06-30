6bd554d27788dffccda1bdf85504f563317968bd
illulli <illu.development@gmail.com>
2020-07-01 08:43:41 +1000

    added error handeling

--- src/utilities/onError.js
+++ src/utilities/onError.js
@@ -1,14 +1,12 @@
-import {sendError, log} from './measure.js';
+import {sendError} from './measure.js';
 
 if(typeof window !== 'undefined'){
     window.onerror = (msg, src, lineno, colno, err) => {
-        if(process.env.GATSBY_MY_ENV === 'production'){
-            //send general to google
-            sendError(msg, true);
-        }else{
-            //print details to console
-            log(`%c ` + msg,  err, 'error', "color: pink");
-        }
+        // if(process.env.GATSBY_MY_ENV === 'production'){
+        //     //send general to google
+        //     sendError(msg, true);
+        // }
+        sendError(msg, true);
         return true;
     }
 }
